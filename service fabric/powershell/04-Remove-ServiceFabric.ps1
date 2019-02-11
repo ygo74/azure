95dbe569460d7bb7efc2be77689cd403693a34f9
ygo74 <yannickgobert@yahoo.fr>
2019-02-11 09:06:17 +0100

    Add Service Fabric

---
+++ service fabric/powershell/04-Remove-ServiceFabric.ps1
@@ -0,0 +1,14 @@
+if ([String]::IsNullOrEmpty($PSScriptRoot)) {
+    $rootScriptPath = "D:\devel\github\devops-toolbox\cloud\azure\service fabric\powershell"
+}
+else {
+    $rootScriptPath = $PSScriptRoot
+}    
+
+$ModulePath = "$rootScriptPath\..\..\powershell\modules\MESF_Azure\MESF_Azure\MESF_Azure.psd1" 
+Import-Module $ModulePath -force
+
+
+& "$rootScriptPath\00-Configuration.ps1"
+
+Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
